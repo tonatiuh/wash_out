8129f23b6c5b33dfdc92a90b2952e63f99f4dde1
Tonatiuh Núñez <tonatiuhnb@gmail.com>
2015-03-03 17:07:27 -0600

    ensure action in controller is executed when it matches with envelope->body->{actionName->{}}

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -26,4 +26,6 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :sorted
 end
--- spec/lib/wash_out/router_spec.rb
+++ spec/lib/wash_out/router_spec.rb
@@ -19,4 +19,37 @@ describe WashOut::Router do
     msg = result[2][0]
     expect(msg).to eq('OK')
   end
+
+  context 'when action name has "Request" suffix' do
+    mock_controller do
+      soap_action 'rumba'
+
+      def rumba
+        render text: 'foobar'
+      end
+    end
+
+    context 'if matching action exists' do
+      let(:action) { 'rumba' }
+
+      it 'executes matching action' do
+        env = {
+          'REQUEST_METHOD' => 'POST',
+          'rack.input' => double('basic-rack-input', {:string => ''}),
+          'wash_out.soap_action' => action,
+          'wash_out.soap_data' => {:Envelope => {:Body => {:rumba => {}}}}
+        }
+
+        response = WashOut::Router.new('Api').call(env)[2]
+        expect(response.body).to eq('foobar')
+      end
+    end
+
+    context 'if no matching action exists' do
+      context 'but matching action without "Request" suffix exists' do
+        it 'executes matching action without "Request" suffix' do
+        end
+      end
+    end
+  end
 end
